8c56d021ef4a9f3ad48e1ee2e3c26ebc4f3521b8
Gary Bezet <gary.bezet@FLUORUSA.LOCAL>
2016-06-21 12:05:48 -0500

    Initial commit

---
+++ CodeJamStandingOvation.java
@@ -0,0 +1,18 @@
+//File:		CodeJamStandingOvation.java
+//Author:	Gary Bezet
+//Date:		2016-06-21
+//Desc:		Designed to solve past google codejam problem "Standing Ovation" qualification round 2015
+//Problem:	https://code.google.com/codejam/contest/6224486/dashboard
+//Results:	N/A
+
+
+public class CodeJamStandingOvation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
